734a4d274186ec635a08cc3218de4aa8df201427
Rob Miller <rob@kalistra.com>
2019-06-06 14:39:19 -0700

    Add setup.py, ignore emacs files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='redash-iodide',
+    version='2019.06.0',
+    packages=['redash_iodide'],
+    license='Mozilla Public License version 2.0',
+    long_description=open('README.rst').read(),
+)
